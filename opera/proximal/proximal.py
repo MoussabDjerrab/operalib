proximal gradient bug fixed

opera/proximal/proximal.py
@@ -18,7 +18,7 @@ def proximal(gradient,norm='L1',mu1=1,mu2=1,partition=None,weight_partition=None
         return prox_l2(gradient,mu2)
     
     elif norm.lower() == 'elasticnet'or norm.lower() == 'elastic net' : 
-        return prox_electicnet(gradient,mu1,mu2)
+        return prox_elasticnet(gradient,mu1,mu2)
     
     elif norm.lower() == 'mixed' or norm.lower() == 'grouplasso'or norm.lower() == 'group lasso' : 
         return prox_grouplasso(gradient,partition,weight_partition)
@@ -48,7 +48,7 @@ def prox_l2(grad,mu):
     '''
     return (1/(1+2*mu))*grad
 
-def prox_electicnet(grad,mu1,mu2):
+def prox_elasticnet(grad,mu1,mu2):
     ''' l1+l2^2-regularization
     Prox_mu(||.||_1+gamma/2||.||_2^2) = Prox_[gammamu/2||.||_2^2] o Prox_[mu||.||_1] = 1/(1+mugamma) Prox_[mu||.||_1]
     '''
@@ -59,7 +59,7 @@ def prox_grouplasso(grad,partition,weights):
     [Prox_mu(u)]_g = ( 1 - mu / ||u_g||_2 )_+ * u_g where g in partition
     '''
     if partition is None : 
-        return prox_lasso(grad,1)
+        return prox_l2(grad,1)
     elif weights is None :
         weights = np.ones(len(partition))
     

opera/proximal/proximalGeneral.py
@@ -2,7 +2,7 @@ import numpy as np
 from proximal import proximal
 import numpy.linalg as LA
 
-def proximalGeneral(L,init=None,objective=None,print_objective=False,maxiters=100,norm='L1',mu1=1,mu2=1,partition=None,weight_partition=None,eps=1.e-3):
+def proximalGeneral(L,init=None,gradient=None,objective=None,print_objective=False,maxiters=100,norm='L1',mu1=1,mu2=1,partition=None,weight_partition=None,eps=1.e-3,backtrack=False):
     """
     init : initial vector (must be given)
     L : Lispsitch coefficient
@@ -16,7 +16,9 @@ def proximalGeneral(L,init=None,objective=None,print_objective=False,maxiters=10
     if init is None : 
         print "FISTA algorithm need an initial vector"
         return None
-    
+    if gradient is None : 
+        print "FISTA algorithm need a gradient function"
+        return None
     # step0 : y1=x0, t1=1
     tk = 1
     yk = init.copy()
@@ -30,9 +32,14 @@ def proximalGeneral(L,init=None,objective=None,print_objective=False,maxiters=10
         #    xk = pl(yk)
         #    t(k+1) = ( 1+sqrt(1+4tk^2) ) / 2
         #    y(k+1) = xk + (tk-1/t(k+1))*(xk-x(k-1))
+        if backtrack :
+            #TODO 
+            L=L
+        
+        
         
         xkOld = xk.copy() 
-        xk = proximal(yk,norm,mu1=mu1/L,mu2=1,partition=partition,weight_partition=weight_partition)
+        xk = proximal(yk-mu1/L*gradient(yk),  norm,mu1=mu1/L,mu2=1,partition=partition,weight_partition=weight_partition)
         
         tkOld = tk
         tk = 0.5*(1 + np.sqrt(1 + 4*tkOld**2))